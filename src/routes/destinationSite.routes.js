f6cd695c3d868d38cbd4ca304a64fe8f2a70e10c
swarnpallavIN074 <swarn.bhaskar@toothsi.in>
2024-05-19 20:05:07 +0530

    fix: imports

--- src/routes/destinationSite.routes.js
+++ src/routes/destinationSite.routes.js
@@ -1,28 +1,16 @@
 import { Router } from "express";
 import {
-	addDestinationSite,
 	getDestinationSiteById,
 	getDestinationsByCityId,
 	like,
 } from "../controllers/destinationSite.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.route("/:id").get(getDestinationSiteById);
 
-router.route("/add").post(
-	upload.fields([
-		{
-			name: "images",
-			maxCount: 10,
-		},
-	]),
-	addDestinationSite
-);
-
 router.route("/getDestinationsByCityId/:id").get(getDestinationsByCityId);
 
-router.route("/like").patch(like)
+router.route("/like").patch(like);
 
 export default router;
--- src/routes/state.routes.js
+++ src/routes/state.routes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { addState, getAllStates } from "../controllers/state.controller.js";
+import { getAllStates } from "../controllers/state.controller.js";
 
 const router = Router();
 
 router.route("/getAllStates").get(getAllStates);
-router.route("/add").post(addState);
 
 export default router;
